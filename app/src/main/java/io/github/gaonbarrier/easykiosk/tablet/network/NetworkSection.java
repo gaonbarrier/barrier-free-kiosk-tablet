06e3368cf41f899cfa3a3b8772290bbf0c585b86
swj1265 <swj1265@naver.com>
2020-07-28 16:23:57 +0900

    What the...

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/network/NetworkSection.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/network/NetworkSection.java
@@ -1,4 +1,8 @@
 package io.github.gaonbarrier.easykiosk.tablet.network;
 
 public class NetworkSection {
+    public NetworkSection(){
+        Receiver receiver = new Receiver();
+        Sender sender = new Sender();
+    }
 }
