8d7a8804d8e9a4d2a2218db5a8c1b80cfa229704
swj1265 <swj1265@naver.com>
2020-07-28 15:53:00 +0900

    init

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/network/Receiver.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/network/Receiver.java
@@ -1,35 +1,64 @@
 package io.github.gaonbarrier.easykiosk.tablet.network;
 
-import android.net.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
 
-public class Receiver {
-    private Socket clientSocket;
-    private DataInputStream clientIn;
-    private DataOutputStream clientOut;
-    private String clientMsg;
+public class Receiver extends Thread {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufReader;
+    private BufferedWriter bufWriter;
 
-    public void joinServer(){
-        new Thread(new Runnable(){
+    public void commandFinder(String json){
 
-            @Override
-            public void run() {
+    }
 
+    public void runServer(){
+        try {
+            // 서버 생성
+            serverSocket = new ServerSocket(2002);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                //client 접속 accept
+                socket = serverSocket.accept();
+                //client가 보낸 데이터 출력
+                bufReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String message = bufReader.readLine();
+                JsonParser parser = new JsonParser();
+                JsonElement element = parser.parse(message);
+                String command = element.getAsJsonObject().get("Action").getAsString();
+                System.out.println("Command : "+ command);
+                System.out.println("Client sent:" + message);
+                if (command == "NewMenu"){
+                    //새로운 메뉴에 대한 작업...
+                    //데이터베이스에 그거 짱박아놓는다
+                }
+                else if(command == "DeleteMenu"){
+                    //메뉴 삭제
+                    //데이터베이스에서 그거 지운다
+                }
+
+                //메시지에 따라서 동작 다르게 하는 법을 연구해봐야 함 ㅇㅇ
+                //if, else if 이용해서 할 생각 ㅇㅇ
+                //client에 데이터 전송
+                bufWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                bufWriter.write(message);
+                bufWriter.newLine();
+                bufWriter.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }).start();
-    }
-
-    public void receiveData() {
-        /*
-        Master아재가 뭔가를 시키면 이제 받아야지?
-        * */
-    }
-    public void sendData(){
-        /*
-        Master 아재한테 주문 데이터 같은 거 보낼 때 필요함.
-        * */
+        }
     }
 }
