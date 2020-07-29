9422068d47a332fa840d16e711fe805b4d4f1ca8
yansigit <44089734+yansigit@users.noreply.github.com>
2020-07-29 14:44:07 +0900

    작동 확인

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NetworkSection nw = new NetworkSection();
+        // NetworkSection nw = new NetworkSection();
     }
 }
