64faa06239e1e08d3b31e16a7861a6a7157b8fcd
seul <hanny1226@naver.com>
2020-07-29 14:22:59 +0900

    버튼이동까지 코드입니다 fragment는 구현중

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
@@ -1,5 +1,7 @@
 package io.github.gaonbarrier.easykiosk.tablet;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this, NormalActivity.class);
+        startActivity(intent);
+    }
+    public void onClick1(View view)
+    {
+        Intent intent = new Intent(this, SoundActivity.class);
+        startActivity(intent);
+    }
+    public void onClick2(View view)
+    {
+        Intent intent = new Intent(this, PracticeActivity.class);
+        startActivity(intent);
     }
 }
