78547ee3076f696895be5d80232c6d8602d76791
swj1265 <swj1265@naver.com>
2020-07-30 16:56:45 +0900

    What the...

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/MainActivity.java
@@ -16,11 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        * 그래서 일단 어떻게 할 생각이냐?
+        * 메뉴 레이아웃, 카트 레이아웃, 결제 레이아웃이 모이는거지
+        * 세 가지의 레이아웃, 그리고 서버와 데이터베이스 파트가 상호작용 하는 것.
+        * 서버랑 데이터베이스 파트는 눈에 안보임 ㅇㅇ
+        *
+        * */
+
         ////////////////////////////////////////
         //Cart나 item 객체 Section
         ////////////////////////////////////////
         Cart = new Cart();
-
 
         /////////////////////////////////////////
         //DB & Server Section
@@ -47,13 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
         예상되는 issue
         ->그래서 어떤 IP에 갓다줄건데?
+        -> 2002번 포트로 그냥 다 쏴버린다? 어케해야할까 이걸...
 
         무언가 받아온다 -> Receiver를 써먹는다.
         얘는 독립된 파츠로써 역할하는 것이라 생각 하는 게 좋을 듯.
         그렇게 움직이도록 설계했음.
 
         계층구조
-        Main Activity > Sender = Receiver = DB
+        Main Activity > Sender = Receiver = DB = others...
         -> 즉 가능하면 Main에서 패러미터를 주는 방식으로 해야 함.
         * */
 
--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/item/menu.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/item/menu.java
@@ -2,9 +2,15 @@ package io.github.gaonbarrier.easykiosk.tablet.item;
 
 import java.util.ArrayList;
 
-public class menu {
-
-    public menu(){
-
-    }
+public class menu {//얘의 목적?
+    //귀찮은 작업을 하기 싫어서?
+    //Menu 레이아웃을 여기다 구현해서 갓다 붙히는게 좋지 않을까???
+    //모듈화 ㄱ
+    /*
+    * 어떻게 해 볼까
+    *
+    * ArrayList Category 내에 coffee, Tea 등등의 카테고리가 있고
+    * coffee, Tea 각각이 각 Menu ArrayList 를 만드는게 좋을까?
+    * 졸라 ㅈ같네...아니야 서버보단 덜 ㅈ같아
+    * */
 }
