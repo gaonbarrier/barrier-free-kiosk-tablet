554d82bc61121f159e1cc9ccdd1e6439dd3d5787
seul <hanny1226@naver.com>
2020-08-10 15:23:18 +0900

    test

--- app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/menu/MenuLayout.java
+++ app/src/main/java/io/github/gaonbarrier/easykiosk/tablet/menu/MenuLayout.java
@@ -15,7 +15,7 @@ public class MenuLayout {//얘의 목적?
     //모듈화 ㄱ
     /*
     * 어떻게 해 볼까
-    *
+    * ㅗㅗ
     * ArrayList Category 내에 coffee, Tea 등등의 카테고리가 있고
     * coffee, Tea 각각이 각 Menu ArrayList 를 만드는게 좋을까?
     * */
@@ -23,17 +23,23 @@ public class MenuLayout {//얘의 목적?
     public MenuLayout(){
         Category = new ArrayList<Category>();
         //커피, 차, 아이스크림 같은 카테고리
+        createCategory();
+
+
+
+    }
+    public void createCategory(){
         ArrayList<String> CategoryName = new ArrayList<>();
         //기계 입장에서는 데베 테이블 던져줘봐야 이게 뭐지? 먹는건가 소리도 못한다. 즉 데베 테이블안에 어떤 카테고리가 있는지
         //알아 볼 필요가 있기 때문에 그 정보들을 저장하는 ArrayList를 만듬
         Cursor c = MainActivity.Receiver.getItemDBOpenHelper().selectColumns();
         while(c.moveToNext()){
-             if(!CategoryName.contains(c.getString(2))) {
-                 CategoryName.add(c.getString(2));
-                 for(String data : CategoryName){
-                     System.out.println(data);
-                 }
-             }
+            if(!CategoryName.contains(c.getString(2))) {
+                CategoryName.add(c.getString(2));
+                for(String data : CategoryName){
+                    System.out.println(data);
+                }
+            }
         }
         //커서안에 짱박혀있는 Item 데이터베이스 아재들을 다 가져와서 카테고리 이름이 어떤 것이 있고 몇개가 있는지 알아내고 뽑아낸다
         //O(n*K), k = 카테고리 갯수 -> 상수취급
@@ -49,23 +55,22 @@ public class MenuLayout {//얘의 목적?
 
             for(item item : temp.getItems()){
                 System.out.println(item.getName() + " , " + item.getCategory() + " , " + item.getHotPrice() + " , " + item.getColdPrice());
-                    ArrayList<Element> tmp = new ArrayList<>();
-                    tmp.add(new Element(item.getName(),"123"));
-                    c = MainActivity.Receiver.getIngredientDBOpenHelper().selectColumns();
-                    while(c.moveToNext()){
-                        if(item.getName().equals(c.getString(1))){
-                            Element element = new Element(c.getString(2),c.getString(3));
-                            tmp.add(element);
-                        }
+                ArrayList<Element> tmp = new ArrayList<>();
+                tmp.add(new Element(item.getName(),"123"));
+                c = MainActivity.Receiver.getIngredientDBOpenHelper().selectColumns();
+                while(c.moveToNext()){
+                    if(item.getName().equals(c.getString(1))){
+                        Element element = new Element(c.getString(2),c.getString(3));
+                        tmp.add(element);
                     }
-                    item.setElements(tmp);
+                }
+                item.setElements(tmp);
                 for(Element Element : item.getElements()){
                     System.out.println(item.getName() + "의 Element 정보 ");
                     System.out.println(Element.getName() + " , " + Element.getImage());
                 }
             }
         }
-
     }
     public void insert(){
 
